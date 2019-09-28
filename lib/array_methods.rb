47e5ae719de3d547b93580206adfd13e23e80318
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-28 18:49:54 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,12 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+def pets = ["dragon","dog"]
+  pets[0]
+  pets[1]
 end
 
-def find_max_value(array)
-  # Add your solution here
+def find_max_value(1,2,3,4,5)
+  find_max_value = |5|
 end
 
-def find_min_value(array)
-  # Add your solution here
+def find_min_value(1,2,3,4,5)
+ find_min_value = |1|
 end
